3f03b65c6b2c74740910d7d71d89d17b23008a6f
Rinat Gareev <gareev-rm@yandex.ru>
2012-12-18 02:45:53 +0400

    +toString implementations in model classes for better logging

--- src/main/scala/ru/kfu/itis/issst/nfcrawler/dao/Article.scala
+++ src/main/scala/ru/kfu/itis/issst/nfcrawler/dao/Article.scala
@@ -5,6 +5,7 @@ package ru.kfu.itis.issst.nfcrawler.dao
 
 import java.net.URL
 import java.util.Date
+import scala.collection.mutable.StringBuilder
 
 /**
  * @author Rinat Gareev (Kazan Federal University)
@@ -15,4 +16,12 @@ class Article(val id: Long, val url: URL, val pubDate: Date, val text: String, v
   require(url != null, "url is null")
   require(text != null, "text is null")
   require(feedId >= 0, "feedID = %s".format(feedId))
+
+  override def toString = new StringBuilder("Article[")
+    .append(id).append(", ")
+    .append(url).append(", ")
+    .append(pubDate).append(", ")
+    .append(feedId).append(", ")
+    .append(if (text == null) text else text.substring(0, Math.min(200, text.length())))
+    .append("]").toString()
 }
--- src/main/scala/ru/kfu/itis/issst/nfcrawler/parser/ParsedFeed.scala
+++ src/main/scala/ru/kfu/itis/issst/nfcrawler/parser/ParsedFeed.scala
@@ -5,11 +5,19 @@ package ru.kfu.itis.issst.nfcrawler.parser
 
 import java.net.URL
 import java.util.Date
+import org.apache.commons.lang3.builder.ToStringBuilder
+import org.apache.commons.lang3.builder.ToStringStyle
 
 /**
  * @author Rinat Gareev (Kazan Federal University)
  *
  */
-class ParsedFeed(val pubDate: Date, val items: List[ParsedFeedItem])
+class ParsedFeed(val pubDate: Date, val items: List[ParsedFeedItem]) {
+  override def toString = new ToStringBuilder(ToStringStyle.SHORT_PREFIX_STYLE)
+    .append(pubDate).append(items).toString()
+}
 
-class ParsedFeedItem(val url: URL, val pubDate: Date)
+class ParsedFeedItem(val url: URL, val pubDate: Date) {
+  override def toString = new ToStringBuilder(ToStringStyle.SHORT_PREFIX_STYLE)
+    .append(url).append(pubDate).toString()
+}
